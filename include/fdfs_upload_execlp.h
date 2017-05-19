e4f7a4f248537c45e33dd24ea474487924e6170c
ZhouJun1997 <ClassZhouJun@163.com>
2017-05-19 20:33:12 +0800

    rm

--- include/fdfs_upload_execlp.h
+++
@@ -1,8 +0,0 @@
-#ifndef _FDFS_UPLOAD_EXECLP_H
-#define _FDFS_UPLOAD_EXECLP_H
-
-#define MAXLINE (1024)
-
-int fdfs_upload_by_filename(const char* filename, char* file_id);
-
-#endif
--- test/make.sh
+++
@@ -1,2 +0,0 @@
-gcc -Wall main.c fdfs_upload_file.c -o my_fdfs_upload -I /usr/include/fastdfs/ -I /usr/include/fastcommon/ -L/usr/lib64/ -lfdfsclient -lfastcommon
-
